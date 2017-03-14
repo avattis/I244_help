8f14fc1f424613e4aeb7a78b16b6f08bb6b27bbc
Annely Vattis <annely.vattis@gmail.com>
2017-03-14 23:22:50 +0200

    muudab värvi kui annan nr, ette, kuidas saan i väärtuse klikiga

--- float.js
+++ float.js
@@ -1,6 +1,23 @@
 
-var list = document.querySelectorAll('div.row div.bead');
+var list = document.querySelectorAll('div.bead');
 var i;
+var len;
+
+/*for (i = 0, len = list.children.lenght; i < len; i = i + 1) {
+    (function (index) {
+    list.children[i].onclick = function () {
+        "use strict";
+        alert(index);
+    };(i);
+}*/
+list[10].onclick = function () {
+    "use strict";
+    for (i = 0; i < list.length; i = i + 1) {
+        list[i].style.backgroundColor = 'red';
+    }
+};
+    
+
 list.onclick = function () {
     "use strict";
     list.style.backgroundColor('red');
